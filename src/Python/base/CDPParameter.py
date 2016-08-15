b53031a58c3b08ccf923dadfbaa8f4d9e87d0f2b
zshaheen <zshaheen93@gmail.com>
2016-08-15 14:41:39 -0700

    Added CDPParameter.py

---
+++ src/Python/base/CDPParameter.py
@@ -0,0 +1,37 @@
+import abc
+import importlib
+import sys
+import os
+
+class CDPParameter(object):
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def check_values():
+        """Check that all of the variables in this parameter file are valid."""
+        raise NotImplementedError()
+
+    def __get__(self):
+        check_values()
+
+    #Parameter files can also be initalized from a file.
+    #This is bad practice, but it was a requirement.
+    def load_parameter_from_py(self, parameter_file_path):
+        #Ex: parameter_file_path is '/Users/shaheen2/Desktop/parameter.py'
+        #path_to_module = '/Users/shaheen2/Desktop'
+        #module_name = 'parameter'
+        #self.user_file is a reference to the parameter file, which is parameter.py in this case
+        #self.user_defined_vars has all of the definded variables such as vars, output_path, etc
+
+        path_to_module = os.path.split(parameter_file_path)[0]
+        #[:-3] used to remove '.py'
+        module_name = os.path.split(parameter_file_path)[1][:-3]
+
+        sys.path.append(path_to_module)
+        self.user_file = importlib.import_module(module_name)
+        self.user_defined_vars = [user_var for user_var in dir(self.user_file) if not user_var.startswith('__')]
+
+        #initalize the variables in this parameter, so the driver can access them as if they were defined regularly
+        #This is the bad practice I was talking about
+        for var in self.user_defined_vars:
+            exec("self." + var + " = self.user_file." + var)
