405e1dfbf0207c0a5b7218e42d92f1ce9ab53030
zshaheen <zshaheen93@gmail.com>
2016-08-15 11:56:03 -0700

    Added CDPDriver.py

---
+++ src/Python/base/CDPDriver.py
@@ -0,0 +1,41 @@
+import abc
+
+class CDPDriver(object):
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self):
+        self.parameter = None
+        self.tools = list()
+
+    def set_parameter(p):
+        self.parameter = p
+
+    def get_parameter():
+        return self.parameter
+
+    def set_tools(self, *tools):
+        self.tools = list(tools)
+
+    def get_tools():
+        return self.tools
+
+
+    @abc.abstractmethod
+    def unpack_parameter():
+        """Check that self.parameter has the correct information for this driver."""
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def run_diags():
+        """Given parameters and tools, run diagnostics."""
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def export():
+        """Export the results from run_diags()."""
+        raise NotImplementedError()
+
+    def run():
+        unpack_parameter()
+        run_diags()
+        export()
