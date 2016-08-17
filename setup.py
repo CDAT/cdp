17ea7f6d3d493b0609131d864b74e193d7337de4
zshaheen <zshaheen93@gmail.com>
2016-08-17 13:57:20 -0700

    setup.py working

---
+++ setup.py
@@ -0,0 +1,12 @@
+import os
+from setuptools import find_packages, setup
+
+setup(
+    name="CDP",
+    version="0.1a",
+    author="Zeshawn Shaheen",
+    author_email="shaheen2@llnl.gov",
+    description="Framework for creating climate diagnostics.",
+    packages=find_packages(),
+    include_package_data=True
+)
