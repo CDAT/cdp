8c37bb9691fa48f56f28981be0a762ebdd9b9407
zshaheen <zshaheen93@gmail.com>
2016-08-24 10:17:06 -0700

    Moved run_diags stuff to a new class

--- CDP/PMP/test/test_PMPDriver.py
+++ CDP/PMP/test/test_PMPDriver.py
@@ -1,25 +1,25 @@
 import unittest
 from CDP.PMP.PMPParameter import *
-from CDP.PMP.PMPDriver import *
+from CDP.PMP.PMPDriverRunDiags import *
 
-class testPMPDriver(unittest.TestCase):
+class testPMPDriverRunDiags(unittest.TestCase):
 
     def setUp(self):
         self.pmp_parameter = PMPParameter()
-        self.pmp_driver = PMPDriver()
+        self.pmp_driver_run_diags = PMPDriverRunDiags(self.pmp_parameter)
 
 
     def test_calculate_level_with_height(self):
         var = ['hus_850']
         var_name_split = var[0].split('_')
-        self.pmp_driver.calculate_level(var_name_split)
-        self.assertEquals(self.pmp_driver.level, 850*100)
+        self.pmp_driver_run_diags.calculate_level(var_name_split)
+        self.assertEquals(self.pmp_driver_run_diags.level, 850*100)
 
     def test_calculate_level_with_no_height(self):
         var = ['hus']
         var_name_split = var[0].split('_')
-        self.pmp_driver.calculate_level(var_name_split)
-        self.assertEquals(self.pmp_driver.level, None)
+        self.pmp_driver_run_diags.calculate_level(var_name_split)
+        self.assertEquals(self.pmp_driver_run_diags.level, None)
 
 
 if __name__ == '__main__':
