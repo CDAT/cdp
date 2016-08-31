81f19f60cd0a712ccd9ba35346ebd97a1e131f77
zshaheen <zshaheen93@gmail.com>
2016-08-31 14:43:00 -0700

    Added write() to PMPIO

--- CDP/PMP/PMPIO.py
+++ CDP/PMP/PMPIO.py
@@ -1,5 +1,11 @@
+import os, logging, json
 import genutil
+import cdat_info
+import cdms2
 from CDP.base.CDPIO import *
+
+class CDMSDomainsEncoder(json.JSONEncoder):
+    pass
 
 
 class PMPIO(CDPIO, genutil.StringConstructor):
@@ -14,7 +20,40 @@ class PMPIO(CDPIO, genutil.StringConstructor):
     def read(self):
         pass
 
-    def write(self):
-        pass
+    def write(self, data_dict, extension='json', *args, **kwargs):
+        file_name = os.path.abspath(self()) + '.' + extension
+        dir_path = os.path.split(file_name)[0]
+
+        if not os.path.exists(dir_path):
+            try:
+                os.makedirs(dir_path)
+            except:
+                logging.error('Could not create output directory: %s' % dir_path)
+                print 'Could not create output directory: %s' % dir_path
+
+        if extension.lower() == 'json':
+            f = open(file_name, 'w')
+            #data_dict['metrics_git_sha1'] = pcmdi_metrics.__git_sha1__
+            data_dict['uvcdat_version'] = cdat_info.get_version()
+            json.dump(data_dict, f, cls=CDMSDomainsEncoder, *args, **kwargs)
+            f.close()
+            logging.info('Results saved to a %s file: %s' % (extension, file_name))
+        elif extension.lower() in ['asc', 'ascii', 'txt']:
+            f = open(file_name, 'w')
+            for key in data_dict.keys():
+                f.write('%s %s\n' % (key, data_dict[key]))
+            f.close()
+            logging.info('Results saved to a %s file: %s' % (extension, file_name))
+        elif extension.lower() == 'nc':
+            f = cdms2.open(file_name, 'w')
+            f.write(data_dict, *args, **kwargs)
+            #f.metrics_git_sha1 = pcmdi_metrics.__git_sha1__
+            f.uvcdat_version = cdat_info.get_version()
+            f.close()
+            logging.info('Results saved to a %s file: %s' % (extension, file_name))
+        else:
+            logging.error('Unknown extension: %s' % extension)
+            raise RuntimeError('Unknown extension: %s' % extension)
 
 thing = PMPIO('root', 'sdfmosmdf')
+thing.write({})
