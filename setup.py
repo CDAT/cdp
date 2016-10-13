990ead6745faef396254023515d278eceab23706
zshaheen <zshaheen93@gmail.com>
2016-10-13 15:22:29 -0700

    Renaming.

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="CDP",
-    version="0.1a",
+    name="cdp",
+    version="0.1",
     author="Zeshawn Shaheen",
     author_email="shaheen2@llnl.gov",
     description="Framework for creating climate diagnostics.",
     packages=find_packages(exclude=["*.test", "*.test.*", "test.*", "test"]),
-    package_data = {'CDP':['PMP/share/*']},
     include_package_data=True
 )
