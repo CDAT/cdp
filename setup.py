345ffe97aa55cf08746c2e309c7d4f268dc5f3c3
zshaheen <zshaheen93@gmail.com>
2016-10-17 11:40:52 -0700

    Prepare for v1.0.0 release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="cdp",
-    version="0.1",
+    version="1.0.0",
     author="Zeshawn Shaheen",
     author_email="shaheen2@llnl.gov",
     description="Framework for creating climate diagnostics.",
