b6a339ba35479e8c0ba3ad96cd7e5dcf6fa364ff
zshaheen <zshaheen93@gmail.com>
2017-05-18 02:09:10 -0700

    viewer consistent

--- cdp/cdp_viewer.py
+++ cdp/cdp_viewer.py
@@ -17,20 +17,23 @@ class OutputViewer(object):
         self.row = None
 
     def add_page(self, name, cols):
-        ''' Add a page to the viewer's index. '''
-        self.page = OutputPage(name, cols)
-        self.pages[name] = self.page
-        self.index.addPage(self.page)
+        """Add a page to the viewer's index"""
+        if name not in self.pages:
+            self.page = OutputPage(name, cols)
+            self.pages[name] = self.page
+            self.index.addPage(self.page)
+        else:
+            self.page = self.pages[name]
 
     def set_page(self, name):
-        ''' Sets the page with the title name as the current page. '''
-        if name in self.pages.keys():
+        """Sets the page with the title name as the current page"""
+        if name in self.pages:
             self.page = self.pages[name]
         else:
             raise RuntimeError('There is no page titled: %s' % name)
 
     def add_group(self, name):
-        ''' Add a group to the current page. '''
+        """Add a group to the current page"""
         if name not in self.groups:
             self.group = OutputGroup(name)
             self.groups[name] = self.group
@@ -39,46 +42,41 @@ class OutputViewer(object):
             self.group = self.groups[name]
 
     def set_group(self, name):
-        ''' Sets the group with the title name as the current group. '''
-        if name in self.groups.keys():
+        """Sets the group with the title name as the current group"""
+        if name in self.groups:
             self.group = self.groups[name]
         else:
             raise RuntimeError('There is no group titled: %s' % name)
 
     def add_row(self, name):
-        ''' Add a row with the title name to the current group. '''
-        cols = []
-        cols.append(name)
-
-        if self.group is None:
-            raise RuntimeError('You must first create a group with add_group()')
-        if self.page is not None:
+        """Add a row with the title name to the current group"""
+        if name not in self.rows:
             self.row = OutputRow(name, [])
             self.rows[name] = self.row
             self.page.addRow(self.row, len(self.page.groups)-1)
         else:
-            raise RuntimeError('You need to add a page with add_page() before calling add_row()')
+            self.row = self.rows[name]
 
     def set_row(self, name):
-        ''' Sets the row with the title name as the current row. '''
-        if name in self.rows.keys():
+        """Sets the row with the title name as the current row"""
+        if name in self.rows:
             self.row = self.rows[name]
         else:
             raise RuntimeError('There is no row titled: %s' % name)
 
     def add_cols(self, cols):
-        ''' Add multiple string cols to the current row. '''
+        """Add multiple string cols to the current row"""
         self.row.columns.append(cols)
 
     def add_col(self, col, is_file=False, **kwargs):
-        ''' Add a single col to the current row. Set is_file to True if the col is a file path. '''
+        """Add a single col to the current row. Set is_file to True if the col is a file path."""
         if is_file:
             self.row.columns.append(OutputFile(col, **kwargs))
         else:
             self.row.columns.append(col)
 
     def generate_viewer(self, prompt_user=True):
-        ''' Generate the webpage and ask the user if they want to see it. '''
+        """Generate the webpage and ask the user if they want to see it."""
         self.index.toJSON(os.path.join(self.path, "index.json"))
 
         default_mask = stat.S_IMODE(os.stat(self.path).st_mode)
