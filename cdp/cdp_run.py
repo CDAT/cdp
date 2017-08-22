a9d422ab417c126e5ecf99a0a329550ac267781a
zshaheen <zshaheen93@gmail.com>
2017-08-22 09:22:42 -0700

    Fix for #19

--- cdp/cdp_run.py
+++ cdp/cdp_run.py
@@ -12,12 +12,14 @@ def serial(func, parameters):
         results.append(func(p))
     return results
 
-def multiprocess(func, parameters):
+def multiprocess(func, parameters, num_workers=None):
     """Run the function with the parameters in parallel using multiprocessing."""
     bag = dask.bag.from_sequence(parameters)
-    
+
     with dask.set_options(get=dask.multiprocessing.get):
-        if hasattr(parameters[0], 'num_workers'):
+        if num_workers:
+            results = bag.map(func).compute(num_workers=num_workers)
+        elif hasattr(parameters[0], 'num_workers'):
             results = bag.map(func).compute(num_workers=parameters[0].num_workers)
         else:
             # num of workers is defaulted to the number of logical processes
@@ -25,19 +27,22 @@ def multiprocess(func, parameters):
 
         return results
 
-def distribute(func, parameters):
+def distribute(func, parameters, scheduler_addr=None):
     """Run the function with the parameters in parallel distributedly."""
     try:
-        if not hasattr(parameters[0], 'scheduler_addr'):
-            raise RuntimeError('scheduler_addr parameter is needed to run diagnostics distributedly.')
+        if scheduler_addr:
+            addr = scheduler_addr
+        elif not hasattr(parameters[0], 'scheduler_addr'):
+            raise RuntimeError('The parameters or distribute() need a scheduler_addr parameter.')
+        else:
+            addr = parameters[0].scheduler_addr
 
-        client = Client(parameters[0].scheduler_addr)
+        client = Client(addr)
         results = client.map(func, parameters)
         client.gather(results)
     except Exception as e:
         print('Distributed run failed.')
-        print(e)
-        return []
+        raise e
     finally:
         client.close()
 
